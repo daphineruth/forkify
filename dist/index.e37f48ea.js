f0111612626c2604b7a808b19a735570d635d939
daphneruth <daphneruth304@gmail.com>
2022-04-08 08:32:50 +0300

    implementing application logic

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -555,107 +555,6 @@ const showRecipe = (async function() {
         const { recipe  } = _modelJs.state;
         //rendering recipe
         _recipeViewJsDefault.default.render(_modelJs.state.recipe);
-        const markup = `
-<figure class="recipe__fig">
- <img src= "${recipe.Image}" alt="${recipe.title}" class="recipe__img" />
-<h1 class="recipe__title">
-  <span>${title}</span>
-</h1>
-</figure>
-
-<div class="recipe__details">
-<div class="recipe__info">
-  <svg class="recipe__info-icon">
-    <use href="${_iconsSvgDefault.default}-clock"></use>
-  </svg>
-  <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
-  <span class="recipe__info-text">minutes</span>
-</div>
-<div class="recipe__info">
-  <svg class="recipe__info-icon">
-    <use href="${_iconsSvgDefault.default}-users"></use>
-  </svg>
-  <span class="recipe__info-data recipe__info-data--people">${recipe.serving}</span>
-  <span class="recipe__info-text">servings</span> 
-  <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${_iconsSvgDefault.default}#icon-minus-circle"></use>
-                </svg>
-              </button>
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${_iconsSvgDefault.default}#icon-plus-circle"></use>
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div class="recipe__user-generated">
-            <svg>
-              <use href="${_iconsSvgDefault.default}#icon-user"></use>
-            </svg>
-          </div>
-          <button class="btn--round">
-            <svg class="">
-              <use href="${_iconsSvgDefault.default}#icon-bookmark-fill"></use>
-            </svg>
-          </button>
-        </div>
-        
-
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Recipe ingredients</h2>
-          <ul class="recipe__ingredient-list">
-          ${recipe.ingredients.map((ing)=>{
-            return `
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${_iconsSvgDefault.default}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${ing.quantity}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-                ${ing.description}
-              </div>
-            </li>
-            `;
-        }).join('')};
-            
-
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${_iconsSvgDefault.default}-check"></use>
-              </svg>
-              <div class="recipe__quantity">0.5</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">cup</span>
-                ricotta cheese
-              </div>
-              </li>
-          </ul>
-        </div>
-
-        <div class="recipe__directions">
-          <h2 class="heading--2">How to cook it</h2>
-          <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
-            directions at their website.
-          </p>
-          <a
-            class="btn--small recipe__btn"
-            href="${recipe.sourceUrl}"
-            target="_blank"
-          >
-            <span>Directions</span>
-            <svg class="search__icon">
-              <use href="${_iconsSvgDefault.default}-arrow-right"></use>
-            </svg>
-          </a>
-        </div>`;
-        recipeContainer.innerHTML = '';
-        recipeContainer.insertAdjacentHTML("afterbegin", markup);
     } catch (err) {
         alert(err);
     }
@@ -1809,6 +1708,109 @@ class RecipeView {
     #data;
     render(data) {
         this.#data = data;
+    }
+     #generateMarkup() {
+        const markup = `
+    <figure class="recipe__fig">
+     <img src= "${recipe.Image}" alt="${recipe.title}" class="recipe__img" />
+    <h1 class="recipe__title">
+      <span>${title}</span>
+    </h1>
+    </figure>
+    
+    <div class="recipe__details">
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icons}-clock"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+      <span class="recipe__info-text">minutes</span>
+    </div>
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icons}-users"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--people">${recipe.serving}</span>
+      <span class="recipe__info-text">servings</span> 
+      <div class="recipe__info-buttons">
+      <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="${icons}#icon-minus-circle"></use>
+                </svg>
+              </button>
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="${icons}#icon-plus-circle"></use>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div class="recipe__user-generated">
+            <svg>
+              <use href="${icons}#icon-user"></use>
+            </svg>
+          </div>
+          <button class="btn--round">
+            <svg class="">
+              <use href="${icons}#icon-bookmark-fill"></use>
+            </svg>
+          </button>
+        </div>
+        
+
+        <div class="recipe__ingredients">
+          <h2 class="heading--2">Recipe ingredients</h2>
+          <ul class="recipe__ingredient-list">
+          ${recipe.ingredients.map((ing)=>{
+            return `
+            <li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="${icons}#icon-check"></use>
+              </svg>
+              <div class="recipe__quantity">${ing.quantity}</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">${ing.unit}</span>
+                ${ing.description}
+              </div>
+            </li>
+            `;
+        }).join('')};
+            
+
+            <li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="${icons}-check"></use>
+              </svg>
+              <div class="recipe__quantity">0.5</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">cup</span>
+                ricotta cheese
+              </div>
+              </li>
+          </ul>
+        </div>
+
+        <div class="recipe__directions">
+          <h2 class="heading--2">How to cook it</h2>
+          <p class="recipe__directions-text">
+            This recipe was carefully designed and tested by
+            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+            directions at their website.
+          </p>
+          <a
+            class="btn--small recipe__btn"
+            href="${recipe.sourceUrl}"
+            target="_blank"
+          >
+            <span>Directions</span>
+            <svg class="search__icon">
+              <use href="${icons}-arrow-right"></use>
+            </svg>
+          </a>
+        </div>`;
+        recipeContainer.innerHTML = '';
+        recipeContainer.insertAdjacentHTML("afterbegin", markup);
     }
 }
 exports.default = new RecipeView();
