c33cc0d4bea12812e4284d7c63330d5553bb9e68
daphneruth <daphneruth304@gmail.com>
2022-04-08 07:58:11 +0300

    modified model.js

--- src/js/model.js
+++ src/js/model.js
@@ -2,19 +2,20 @@
     recipe: {}
 };
 
-export const loadRecipe = async function(){
-    const res = await fetch(` https://forkify-api.herokuapp.com/api/v2/recipes/${id}>`
+export const loadRecipe = async function(id){
+    const res = await fetch(
+        ` https://forkify-api.herokuapp.com/api/v2/recipes/${id}>`
     );
     const data = await res.json();
 
     if (res.ok) throw new Error(`${data.message} (${res.status})`);
       
     
-    console.log (res, data);
+    const { recipe } = data.data;
 
    // let (recipe) = data.data;
 
-    recipe = {
+    state.recipe = {
       id: recipe.id,
       title: recipe.title,
       publisher: recipe.publisher,
