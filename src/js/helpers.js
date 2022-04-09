4f4fb4dc9ce5e22fdca0394f41860b3af88b046a
daphneruth <daphneruth304@gmail.com>
2022-04-09 17:05:58 +0300

    updated

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -9,14 +9,14 @@ const timeout = function (s) {
 
 export const getJSON = async function(url) {
     try{
-    const res = await fetch(
-        `${API_URL}/${url}>`
-    );
+        
+    const res = await Promise.race([fetch(url), timeout(5)]);
     const data = await res.json();
 
     if (res.ok) throw new Error(`${data.message} (${res.status})`);
     return data;
-}catch(err){
+}
+catch(err){
 throw err;
 }
 }
