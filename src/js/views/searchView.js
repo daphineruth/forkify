dba812007807468aef02914bb70b2aa8b065e6cb
daphneruth <daphneruth304@gmail.com>
2022-04-10 22:50:10 +0300

    updated

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -5,7 +5,7 @@ class SearchView {
   
     getQuery() {
       const query = this._parentEl.querySelector('.search__field').value;
-      this._clearInput();
+      this.#clearInput();
       return query;
     }
 
