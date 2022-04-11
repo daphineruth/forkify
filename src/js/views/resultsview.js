0a7ae64f3277df373bf3fd4bdb0fea01656b1886
daphneruth <daphneruth304@gmail.com>
2022-04-11 21:44:22 +0300

    fixed some bug

--- src/js/views/resultsview.js
+++ src/js/views/resultsview.js
@@ -2,7 +2,7 @@ import View from "./View";
 
 class resultsView  {
 
-    _parentEL = document.querySelector('.results')
+    _parentElement = document.querySelector('.results')
 }
 
 export default new resultsView();
