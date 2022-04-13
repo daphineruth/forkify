508f6922e6ee760403e1b2abbbf403c750dd2d76
daphneruth <daphneruth304@gmail.com>
2022-04-13 17:57:21 +0300

    setting default page

--- src/js/model.js
+++ src/js/model.js
@@ -8,6 +8,7 @@ import { state } from '../../../starter fle/complete-javascript-course/18-forkif
     search: {
         query: '',
         results: [],
+        page:1,
         resultsPerPage : RES_PER_PAGE,
     },
 
@@ -63,8 +64,10 @@ export const loadSearchResults = async function (query) {
     }
 }
  //loadSearchResults('pizza')
- export const getSearchResultsPage = function(page)
- {  
+ export const getSearchResultsPage = function(page = state.search.page ) {
+
+ state.search.page = page;
+  
      const start = (page - 1)* state.search.resultsPerPage;//0;
      const end  =page * state.search.resultsPerPage; //9;
 
