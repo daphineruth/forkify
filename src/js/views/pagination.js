7e33a088617cf56a043c213aad18fefb6ca63aa9
daphneruth <daphneruth304@gmail.com>
2022-04-14 06:03:25 +0300

    calculating the number of pages

--- src/js/views/pagination.js
+++ src/js/views/pagination.js
@@ -3,10 +3,13 @@ import view from './View'
 import icons  from 'url:../../img/icons.svg';
 
 class PaginationView extends View  {
-    _parentElement = document.querySelector('."pagination')
+    _parentElement = document.querySelector('."pagination');
+
+    _generateMarkup(){
+
+        const numPages = this._data.results / this.data. resultsPerPage;
+    }
 }
+
 
 export  default PaginationView()
-{
-
-}
