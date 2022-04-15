b0d42d4e1f0178a1810bed70197ae63953789cc0
daphneruth <daphneruth304@gmail.com>
2022-04-15 04:47:07 +0300

    implementig pagination

--- src/js/views/pagination.js
+++ src/js/views/pagination.js
@@ -8,6 +8,24 @@ class PaginationView extends View  {
     _generateMarkup(){
 
         const numPages = math.ceil(this._data.results.length / this.data. resultsPerPage);
+
+        //first page and there are other pages
+        if (this._data.page === 1 && numPages > 1){
+            return 'page 1 others'
+        }
+
+       //first page and there are no other pages
+       return'only one page'
+
+       //last page
+       if (this._data.page === numPages && numPages > 1){
+           return 'last page'
+       }
+
+       //other pages
+       if (this._data.page < numPages) {
+           return 'other page'
+       }
     }
 }
 
