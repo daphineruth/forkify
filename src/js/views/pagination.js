3ba9dcfbff6470157c0110f2c1d6846e413c4d85
daphneruth <daphneruth304@gmail.com>
2022-04-15 04:56:45 +0300

    added previous page markuo

--- src/js/views/pagination.js
+++ src/js/views/pagination.js
@@ -6,11 +6,12 @@ class PaginationView extends View  {
     _parentElement = document.querySelector('."pagination');
 
     _generateMarkup(){
+        const curPage = this._data.page;
 
-        const numPages = math.ceil(this._data.results.length / this.data. resultsPerPage);
+         const numPages = math.ceil(this._data.results.length / this.data. resultsPerPage);
 
         //first page and there are other pages
-        if (this._data.page === 1 && numPages > 1){
+        if (curPage === 1 && numPages > 1){
             return 'page 1 others'
         }
 
@@ -18,12 +19,19 @@ class PaginationView extends View  {
        return'only one page'
 
        //last page
-       if (this._data.page === numPages && numPages > 1){
-           return 'last page'
+       if (curPage === numPages && numPages > 1){
+           return `
+           <button class="btn--inline pagination__btn--prev">
+           <svg class="search__icon">
+             <use href="${icons}#icon-arrow-left"></use>
+           </svg>
+           <span>Page ${curPage - 1}</span>
+         </button>
+         `;
        }
 
        //other pages
-       if (this._data.page < numPages) {
+       if (curPage < numPages) {
            return 'other page'
        }
     }
