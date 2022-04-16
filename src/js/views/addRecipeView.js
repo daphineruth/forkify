48fed5d189afcad0a5c5d86634d7c3d3b4557968
daphneruth <daphneruth304@gmail.com>
2022-04-16 06:49:24 +0300

    updated

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -13,12 +13,25 @@ class AddRecipeView extends View {
   constructor() {
     super();
     this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
     
+  }
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
   }
 
   _addHandlerShowWindow() {
     this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
   }
+
+  
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  
 
   _generateMarkup() {}
 }
